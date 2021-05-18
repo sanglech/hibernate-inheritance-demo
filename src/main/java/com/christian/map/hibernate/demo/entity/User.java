d874aa3c6f8692a8416e825a14df126307bbbe25
Christian <christian@sanglech.com>
2021-05-19 00:31:12 +0900

    Joined Table Demo

--- src/main/java/com/christian/map/hibernate/demo/entity/User.java
+++ src/main/java/com/christian/map/hibernate/demo/entity/User.java
@@ -5,10 +5,10 @@ import javax.persistence.*;
 
 @Entity
 @Table(name="user")
-@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Inheritance(strategy = InheritanceType.JOINED)
 public abstract class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.TABLE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(name = "first_name")
